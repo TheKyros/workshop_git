6e1641a3bf55d8ac49cd10abcc12b015016609e8
cyrilblain <cyrilblain@hotmail.fr>
2024-05-01 20:10:18 +0200

    feat: add log function in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,13 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-app';
+  myProperty = 'ceci est ma propriété';
+
+  private logMyProperty() {
+    console.log(this.myProperty);
+  }
 }
